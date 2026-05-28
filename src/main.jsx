c191ca52af7b2102ad58ae702d0ef2ab07622f80
closermatheus-creator <closermatheus@gmail.com>
2026-05-28 20:26:16 -0300

    mais jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,23 +8,21 @@ import { AuthProvider } from './contexts/AuthContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <AuthProvider>
-          <Toaster
-            position="top-right"
-            toastOptions={{
-              style: {
-                background: 'var(--bg-secondary)',
-                color: 'var(--text-primary)',
-                border: '1px solid var(--border-color)',
-              },
-            }}
-          />
-          <App />
-        </AuthProvider>
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <ThemeProvider>
+      <AuthProvider>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            style: {
+              background: 'var(--bg-secondary)',
+              color: 'var(--text-primary)',
+              border: '1px solid var(--border-color)',
+            },
+          }}
+        />
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
+  </BrowserRouter>,
 )
